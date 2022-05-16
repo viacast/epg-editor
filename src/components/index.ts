9fb89e2cd244a601685bd34e690a08c37b00390a
gusbmachado <gustavobmachado105@gmail.com>
2022-05-16 11:50:52 -0300

    update components export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
+import Import from './Import';
+import Export from './Export';
+import Add from './Add';
+import Header from './Header';
 import ToastContainer from './ToastContainer';
 
 // eslint-disable-next-line import/prefer-default-export
-export { ToastContainer };
+export { Import, Export, Add, Header, ToastContainer };
