afbf64c49231914d00781843094bd7a0948f9252
gusbmachado <gustavobmachado105@gmail.com>
2022-05-16 16:08:09 -0300

    header element update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import Export from 'components/Export';
 import Import from 'components/Import';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Container, Form, Text } from './styles';
 
 const Header: React.FC = () => {
   const { t } = useTranslation();
@@ -15,39 +16,20 @@ const Header: React.FC = () => {
     setNewUserInfo({ ...newUserInfo, profileImages: f });
 
   return (
-    <div style={{ paddingTop: '40px', paddingLeft: '25px', width: '100%' }}>
-      <form style={{ display: 'flex' }}>
+    <Container>
+      <Form>
         <Import
           accept=".jpg,.png,.jpeg"
           label="Profile Image(s)"
           multiple
           updateFilesCb={updateUploadedFiles}
         />
-        &emsp;
-        <Export
-          accept=".jpg,.png,.jpeg"
-          label="Profile Image(s)"
-          multiple
-          updateFilesCb={updateUploadedFiles}
-        />
-        &emsp; &emsp;
-        <p
-          style={{
-            position: 'relative',
-            top: '15px',
-            verticalAlign: 'middle',
-            fontFamily: 'Nunito, sans-serif',
-            color: 'var(--color-neutral-3)',
-            fontSize: '20px',
-            fontStyle: ' ExtraLight 200',
-          }}
-        >
-          Programs: 19
-        </p>
-        &emsp; &emsp;
-        <Add updateFilesCb={undefined} />
-      </form>
-    </div>
+        &nbsp;&nbsp;
+        <Export />
+        <Text>Programs: 19</Text>
+        <Add />
+      </Form>
+    </Container>
   );
 };
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,1 +1,35 @@
-export {};
+import styled from 'styled-components';
+import 'styles/global';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Form = styled.form`
+  margin-bottom: 15px;
+  display: flex;
+  width: 935px;
+  font-family: Nunito, sans-serif;
+  color: var(--color-neutral-3);
+  font-size: 20px;
+  font-style: Regular;
+  font-weight: 400;
+`;
+
+export const Container = styled.div`
+  padding-top: 40px;
+  padding-left: 25px;
+  width: 935px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+  @media screen and (min-width: 940px) {
+    overflow: hidden;
+  }
+`;
+
+export const Text = styled.p`
+  user-select: none;
+  margin-left: 31px;
+  margin-right: 31px;
+  position: relative;
+  top: 12px;
+  bottom: 12px;
+`;
