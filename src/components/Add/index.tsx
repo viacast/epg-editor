cee36d5f6d380291fa6950a1c3580ed98d309070
gusbmachado <gustavobmachado105@gmail.com>
2022-05-16 16:07:52 -0300

    add  button update

--- src/components/Add/index.tsx
+++ src/components/Add/index.tsx
@@ -1,26 +1,13 @@
-import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { FormField, ExportFileBtn } from './styles';
+import { AddBtn } from './styles';
 
-const Add = ({ ...otherProps }) => {
-  const fileInputField = useRef(null);
-
+const Add = () => {
   return (
-    <>
-      <ExportFileBtn type="button">
-        <FontAwesomeIcon className="icon" icon={faPlus} />
-        <span> Add {otherProps.multiple ? 'files' : 'a file'}</span>
-      </ExportFileBtn>
-      <FormField
-        type="file"
-        ref={fileInputField}
-        title=""
-        value=""
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...otherProps}
-      />
-    </>
+    <AddBtn type="button">
+      <FontAwesomeIcon className="icon" icon={faPlus} />
+      <span> Add </span>
+    </AddBtn>
   );
 };
 
--- src/components/Add/styles.ts
+++ src/components/Add/styles.ts
@@ -1,40 +1,8 @@
 import styled from 'styled-components';
 import 'styles/global';
 
-export const FormField = styled.input`
-  font-size: 18px;
-  display: block;
-  width: 100%;
-  border: none;
-  text-transform: none;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  opacity: 0;
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-export const InputLabel = styled.label`
-  top: -21px;
-  font-size: 13px;
-  color: black;
-  left: 0;
-  position: absolute;
-`;
-
-export const DragDropText = styled.p`
-  font-weight: bold;
-  letter-spacing: 2.2px;
-  margin-top: 0;
-  text-align: center;
-`;
-
-export const ExportFileBtn = styled.button`
+// eslint-disable-next-line import/prefer-default-export
+export const AddBtn = styled.button`
   box-sizing: border-box;
   appearance: none;
   background-color: transparent;
@@ -44,59 +12,43 @@ export const ExportFileBtn = styled.button`
   line-height: 1;
   padding: 1.1em 2.8em;
   text-align: center;
-  text-transform: uppercase;
   font-weight: 700;
   border-radius: 2px;
   color: var(--color-neutral-3);
   position: relative;
   overflow: hidden;
   z-index: 1;
-  transition: color 250ms ease-in-out;
   font-family: 'Nunito', sans-serif;
-  width: 12%;
+  width: 139px;
+  height: 44px;
   display: flex;
   align-items: center;
   padding-right: 0;
   justify-content: center;
 
-  &:after {
-    content: '';
-    position: absolute;
-    display: block;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 0;
-    height: 100%;
-    background: var(--color-system-3);
-    z-index: -1;
-    transition: width 250ms ease-in-out;
-  }
-
   .icon,
   i {
-    font-size: 22px;
-    margin-top: 15px;
-    margin-right: 5px;
+    font-size: 20px;
+    padding-right: 13px;
     border-right: 2px solid;
-    position: absolute;
     vertical-align: middle;
     top: 0;
     bottom: 0;
     left: 0;
     right: 0;
-    width: 20%;
+    width: 20px;
     display: flex;
     flex-direction: column;
     justify-content: center;
   }
 
-  @media only screen and (max-width: 500px) {
-    width: 70%;
-  }
-
-  @media only screen and (max-width: 350px) {
-    width: 100%;
+  span {
+    margin-left: 30px;
+    margin-right: 60px;
+    font-family: Nunito, sans-serif;
+    font-style: Regular;
+    font-weight: 400;
+    font-size: 16px;
   }
 
   &:hover {
@@ -104,106 +56,10 @@ export const ExportFileBtn = styled.button`
     border: 2px solid var(--color-neutral-2);
     outline: 0;
     background: transparent;
-
-    &:after {
-      width: 110%;
-    }
   }
 
   &:focus {
     outline: 0;
     background: transparent;
   }
-
-  &:disabled {
-    opacity: 0.4;
-    filter: grayscale(100%);
-    pointer-events: none;
-  }
-`;
-
-export const FilePreviewContainer = styled.article`
-  margin-bottom: 35px;
-
-  span {
-    font-size: 14px;
-  }
-`;
-
-export const PreviewList = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 10px;
-
-  @media only screen and (max-width: 400px) {
-    flex-direction: column;
-  }
-`;
-
-export const FileMetaData = styled.div`
-  flex-direction: column;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  padding: 10px;
-  border-radius: 2px;
-  color: white;
-  font-weight: bold;
-  background-color: rgba(5, 5, 5, 0.55);
-
-  aside {
-    margin-top: auto;
-    display: flex;
-    justify-content: space-between;
-  }
-`;
-
-export const RemoveFileIcon = styled.i`
-  cursor: pointer;
-
-  &:hover {
-    transform: scale(1.3);
-  }
-`;
-
-export const PreviewContainer = styled.section`
-  padding: 0.25rem;
-  width: 20%;
-  height: 44px;
-  border-radius: 2px;
-  box-sizing: border-box;
-
-  &:hover {
-    opacity: 0.55;
-
-    ${FileMetaData} {
-      display: flex;
-    }
-  }
-
-  & > div:first-of-type {
-    height: 100%;
-    position: relative;
-  }
-
-  @media only screen and (max-width: 750px) {
-    width: 25%;
-  }
-
-  @media only screen and (max-width: 500px) {
-    width: 50%;
-  }
-
-  @media only screen and (max-width: 400px) {
-    width: 100%;
-    padding: 0 0 0.4em;
-  }
-`;
-
-export const ImagePreview = styled.img`
-  border-radius: 2px;
-  width: 100%;
-  height: 100%;
 `;
