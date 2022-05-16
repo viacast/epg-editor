55f524aa4537712edb4c4103bd713c3b46780a60
gusbmachado <gustavobmachado105@gmail.com>
2022-05-16 11:52:30 -0300

    update home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,15 @@
-import Import from 'components/Import';
-import React, { useState } from 'react';
+import Header from 'components/Header';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Container } from './styles';
 
 const Home: React.FC = () => {
   const { t } = useTranslation();
 
-  const [newUserInfo, setNewUserInfo] = useState({
-    profileImages: [],
-  });
-
-  const updateUploadedFiles = f =>
-    setNewUserInfo({ ...newUserInfo, profileImages: f });
-
   return (
-    <div>
-      <form>
-        <Import
-          accept=".jpg,.png,.jpeg"
-          label="Profile Image(s)"
-          multiple
-          updateFilesCb={updateUploadedFiles}
-        />
-      </form>
-    </div>
+    <Container>
+      <Header />
+    </Container>
   );
 };
 
