1912afd66adb01351a90b0d36576792e656e4a1c
gusbmachado <gustavobmachado105@gmail.com>
2022-05-20 08:05:23 -0300

    header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,91 @@
-import Add from 'components/Add';
-import Export from 'components/Export';
-import Import from 'components/Import';
-import React, { useState } from 'react';
+import BaseButton from 'components/Button';
+import React, { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Container, Form, Text } from './styles';
+import AddIcon from '@mui/icons-material/Add';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import MainInput from 'components/Input';
+import { FormField } from 'components/Button/styles';
+import { Container, Form, Select, Text } from './styles';
 
-const Header: React.FC = () => {
-  const { t } = useTranslation();
+const Header: React.FC = ({ ...otherProps }) => {
+  const { t, i18n } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
+  const [programCount, setProgramCount] = useState(0);
 
-  const [newUserInfo, setNewUserInfo] = useState({
-    profileImages: [],
-  });
+  const handleChange = evt => {
+    const opt = evt.target.value;
+    setCurrentLanguage(opt);
+    i18n.changeLanguage(opt);
+  };
 
-  const updateUploadedFiles = f =>
-    setNewUserInfo({ ...newUserInfo, profileImages: f });
+  // ################################################## //
+
+  const fileInputField = useRef(null);
+  const [files, setFiles] = useState({});
+
+  const maxFileSizeInBytes = 500000;
+
+  const addNewFiles = newFiles => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const file of newFiles) {
+      if (file.size <= maxFileSizeInBytes) {
+        if (!otherProps) {
+          return { file };
+        }
+        files[file.name] = file;
+      }
+    }
+    return { ...files };
+  };
+
+  const handleNewFileUpload = e => {
+    const { files: newFiles } = e.target;
+    if (newFiles.length) {
+      const updatedFiles = addNewFiles(newFiles);
+      setFiles(updatedFiles);
+    }
+  };
+
+  // ################################################## //
 
   return (
     <Container>
       <Form>
-        <Import
-          accept=".jpg,.png,.jpeg"
-          label="Profile Image(s)"
-          multiple
-          updateFilesCb={updateUploadedFiles}
+        <MainInput />
+        <div
+          style={{ width: 'auto', height: 'auto', paddingRight: '1.672241%' }}
+        >
+          <BaseButton
+            text={`${t('header:buttonImportProgram')}`}
+            icon={<FileDownloadIcon />}
+          />
+          <FormField
+            type="file"
+            ref={fileInputField}
+            onChange={handleNewFileUpload}
+            title=""
+            value=""
+          />
+        </div>
+        <BaseButton
+          text={`${t('header:buttonExportProgram')}`}
+          icon={<UploadFileIcon />}
         />
-        &nbsp;&nbsp;
-        <Export />
-        <Text>Programs: 19</Text>
-        <Add />
+        <Text>
+          {t('header:labelProgram', {
+            count: programCount,
+          })}
+        </Text>
+        <BaseButton
+          text={`${t('header:buttonAddProgram')}`}
+          icon={<AddIcon />}
+          onClick={() => setProgramCount(c => c + 1)}
+        />
+        <Select onChange={handleChange} value={currentLanguage}>
+          <option value="pt">Portugues</option>
+          <option value="en">English</option>
+        </Select>
       </Form>
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,35 +1,51 @@
 import styled from 'styled-components';
 import 'styles/global';
 
-// eslint-disable-next-line import/prefer-default-export
-export const Form = styled.form`
-  margin-bottom: 15px;
-  display: flex;
-  width: 935px;
-  font-family: Nunito, sans-serif;
-  color: var(--color-neutral-3);
-  font-size: 20px;
-  font-style: Regular;
-  font-weight: 400;
-`;
-
 export const Container = styled.div`
   padding-top: 40px;
   padding-left: 25px;
-  width: 935px;
-  overflow-x: auto;
-  overflow-y: hidden;
-  white-space: nowrap;
-  @media screen and (min-width: 940px) {
-    overflow: hidden;
-  }
+  width: fit-content; //923px; //100%;
+  /* @media only screen and (max-width: 600px) {
+    overflow-x: auto;
+    overflow-y: hidden;
+    white-space: nowrap;
+  } */
 `;
 
-export const Text = styled.p`
+export const Form = styled.form`
+  display: flex;
+  width: fit-content; //897px; //46.71875%;
+  font-family: Nunito, sans-serif;
+  color: var(--color-neutral-3);
+`;
+
+export const Text = styled.div`
+  width: fit-content;
+  height: 44px;
+  white-space: nowrap;
+  font-size: 20px;
   user-select: none;
-  margin-left: 31px;
-  margin-right: 31px;
+  margin-left: 3.455964%;
+  margin-right: 3.455964%;
   position: relative;
-  top: 12px;
-  bottom: 12px;
+  padding: 10px 0;
+`;
+
+export const Select = styled.select`
+  position: relative;
+  top: 0px;
+  left: 15px;
+  padding: 5px 5px 5px;
+  border-radius: 2px;
+  outline: 0;
+  width: 140px;
+  height: 44px;
+  transition: border-color 0.2s;
+  border: none;
+  box-shadow: none;
+  font-family: Nunito, sans-serif;
+  font-size: 18px;
+  cursor: pointer;
+  background: var(--color-neutral-6);
+  color: var(--color-neutral-3);
 `;
