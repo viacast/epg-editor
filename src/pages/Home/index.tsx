b90a3318084b7969383697f853f77bba3c94ec37
gusbmachado <gustavobmachado105@gmail.com>
2022-05-25 12:41:00 -0300

    Page Home Update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,8 @@
 import Header from 'components/Header';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { Container } from './styles';
 
 const Home: React.FC = () => {
-  const { t } = useTranslation();
-
   return (
     <Container>
       <Header />
