bfa42d8ad35462d97bddc252eabb5b20e9a71e71
gusbmachado <gustavobmachado105@gmail.com>
2022-05-25 12:37:24 -0300

    Input Componet Update

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Stack from '@mui/material/Stack';
 import 'styles/global';
 import TextField from '@mui/material/TextField';
 import { InputProps } from '@mui/material/Input';
-import Box from '@mui/material/Box';
+import CloseIcon from '@mui/icons-material/Close';
+import { Divider, IconButton, Paper } from '@mui/material';
 
 export interface StyledInput {
+  disabled: boolean;
   value: string;
+  placeholder: string;
+  // eslint-disable-next-line react/require-default-props
+  setValue?: (value: string) => void;
 }
 
 const Input = styled(TextField)<InputProps>(() => ({
@@ -17,6 +21,7 @@ const Input = styled(TextField)<InputProps>(() => ({
   textAlign: 'center',
   verticalAlign: 'middle',
   cursor: 'pointer',
+  userSelect: 'none',
   textTransform: 'none',
   fontSize: 20,
   borderRadius: '2px',
@@ -24,32 +29,54 @@ const Input = styled(TextField)<InputProps>(() => ({
   fontFamily: 'Nunito, sans-serif',
 }));
 
-const MainInput: React.FC<StyledInput> = ({ value }) => {
+const MainInput: React.FC<StyledInput> = ({
+  disabled,
+  value,
+  placeholder,
+  setValue,
+}) => {
   return (
-    <Stack spacing={2} direction="row" width="270px" marginRight="1.337793%">
-      <Box>
-        <Input
-          disabled
-          fullWidth
-          value={value}
-          inputProps={{
-            style: {
-              height: '11px',
+    <Paper
+      component="form"
+      sx={{
+        p: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: '270px',
+        height: '44px',
+        marginRight: '12px',
+        backgroundColor: 'var(--color-neutral-6)',
+      }}
+    >
+      <Input
+        disabled={disabled}
+        fullWidth
+        value={value}
+        placeholder={placeholder}
+        inputProps={{
+          style: {
+            height: '11px',
+          },
+        }}
+        sx={{
+          '& .MuiInputBase-root.Mui-disabled': {
+            '& > fieldset': {
+              borderColor: 'var(--color-neutral-6)',
             },
-          }}
-          sx={{
-            '& .MuiInputBase-root.Mui-disabled': {
-              '& > fieldset': {
-                borderColor: 'var(--color-neutral-6)',
-              },
-            },
-            '& .MuiInputBase-input.Mui-disabled': {
-              WebkitTextFillColor: 'var(--color-neutral-3)',
-            },
-          }}
-        />
-      </Box>
-    </Stack>
+          },
+          '& .MuiInputBase-input.Mui-disabled': {
+            WebkitTextFillColor: 'var(--color-neutral-3)',
+          },
+        }}
+      />
+      <Divider sx={{ height: 28, m: 1 }} orientation="vertical" />
+      <IconButton
+        sx={{ p: '10px', color: 'var(--color-neutral-3)' }}
+        aria-label="directions"
+      >
+        <CloseIcon onClick={() => setValue?.('')} />
+      </IconButton>
+    </Paper>
   );
 };
 
