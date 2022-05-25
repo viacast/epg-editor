a0d94b6d8bef0426b29a3951da2664f51b092bc7
gusbmachado <gustavobmachado105@gmail.com>
2022-05-25 12:41:00 -0300

    Page Home Update

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,3 +5,10 @@ export const Container = styled.div`
   display: flex;
   flex: 1;
 `;
+
+export const Form = styled.form`
+  display: flex;
+  width: fit-content; //897px; //46.71875%;
+  font-family: Nunito, sans-serif;
+  color: var(--color-neutral-3);
+`;
