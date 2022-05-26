de86c6f217f30d8774165f5fe37ca0c2d5cb4f51
gusbmachado <gustavobmachado105@gmail.com>
2022-05-26 14:59:10 -0300

    Header Component Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,46 +1,47 @@
 import BaseButton from 'components/Button';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import AddIcon from '@mui/icons-material/Add';
-import UploadFileIcon from '@mui/icons-material/UploadFile';
+import { RiMenuAddFill } from 'react-icons/ri';
+import { FaFileExport } from 'react-icons/fa';
 import InputButton from 'components/InputButton';
-import { Container, Form, Select, Text } from './styles';
+import { Container as HeaderContainer, Select, Text } from './styles';
 
 const Header: React.FC = () => {
   const { t, i18n } = useTranslation();
   const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
-  const [programCount, setProgramCount] = useState(19);
+  const [programCount, setProgramCount] = useState(0);
 
-  const handleChange = evt => {
-    const opt = evt.target.value;
-    setCurrentLanguage(opt);
-    i18n.changeLanguage(opt);
-  };
+  const handleChange = useCallback(
+    evt => {
+      const { value } = evt.target;
+      setCurrentLanguage(value);
+      i18n.changeLanguage(value);
+    },
+    [i18n],
+  );
 
   return (
-    <Container>
-      <Form>
-        <InputButton />
-        <BaseButton
-          text={`${t('header:buttonExportProgram')}`}
-          icon={<UploadFileIcon />}
-        />
-        <Text>
-          {t('header:labelProgram', {
-            count: programCount,
-          })}
-        </Text>
-        <BaseButton
-          text={`${t('header:buttonAddProgram')}`}
-          icon={<AddIcon />}
-          onClick={() => setProgramCount(c => c + 1)}
-        />
-        <Select onChange={handleChange} value={currentLanguage}>
-          <option value="pt">Portugues</option>
-          <option value="en">English</option>
-        </Select>
-      </Form>
-    </Container>
+    <HeaderContainer className="no-user-select">
+      <InputButton />
+      <BaseButton
+        text={`${t('header:buttonExportProgram')}`}
+        icon={<FaFileExport />}
+      />
+      <Text>
+        {t('header:labelProgram', {
+          count: programCount,
+        })}
+      </Text>
+      <BaseButton
+        text={`${t('header:buttonAddProgram')}`}
+        icon={<RiMenuAddFill />}
+        onClick={() => setProgramCount(c => c + 1)}
+      />
+      <Select onChange={handleChange} value={currentLanguage}>
+        <option value="pt">Portugues</option>
+        <option value="en">English</option>
+      </Select>
+    </HeaderContainer>
   );
 };
 
