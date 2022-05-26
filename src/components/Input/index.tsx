3913f888ef2ed3b99b7bf23ff6bf445eb7f0a17c
gusbmachado <gustavobmachado105@gmail.com>
2022-05-26 14:58:42 -0300

    Input Component Update

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import 'styles/global';
-import TextField from '@mui/material/TextField';
-import { InputProps } from '@mui/material/Input';
-import CloseIcon from '@mui/icons-material/Close';
-import { Divider, IconButton, Paper } from '@mui/material';
+import { CgClose } from 'react-icons/cg';
+import {
+  StyledDivider,
+  StyledIconButton,
+  StyledPaper,
+  StyledInput,
+} from './styles';
 
-export interface StyledInput {
+export interface InputProps {
   disabled: boolean;
   value: string;
   placeholder: string;
@@ -14,41 +16,15 @@ export interface StyledInput {
   setValue?: (value: string) => void;
 }
 
-const Input = styled(TextField)<InputProps>(() => ({
-  width: '270px',
-  height: '44px',
-  display: 'flex',
-  textAlign: 'center',
-  verticalAlign: 'middle',
-  cursor: 'pointer',
-  userSelect: 'none',
-  textTransform: 'none',
-  fontSize: 20,
-  borderRadius: '2px',
-  backgroundColor: 'var(--color-neutral-6)',
-  fontFamily: 'Nunito, sans-serif',
-}));
-
-const MainInput: React.FC<StyledInput> = ({
+const Input: React.FC<InputProps> = ({
   disabled,
   value,
   placeholder,
   setValue,
 }) => {
   return (
-    <Paper
-      component="form"
-      sx={{
-        p: '2px 4px',
-        display: 'flex',
-        alignItems: 'center',
-        width: '270px',
-        height: '44px',
-        marginRight: '12px',
-        backgroundColor: 'var(--color-neutral-6)',
-      }}
-    >
-      <Input
+    <StyledPaper>
+      <StyledInput
         disabled={disabled}
         fullWidth
         value={value}
@@ -58,26 +34,13 @@ const MainInput: React.FC<StyledInput> = ({
             height: '11px',
           },
         }}
-        sx={{
-          '& .MuiInputBase-root.Mui-disabled': {
-            '& > fieldset': {
-              borderColor: 'var(--color-neutral-6)',
-            },
-          },
-          '& .MuiInputBase-input.Mui-disabled': {
-            WebkitTextFillColor: 'var(--color-neutral-3)',
-          },
-        }}
       />
-      <Divider sx={{ height: 28, m: 1 }} orientation="vertical" />
-      <IconButton
-        sx={{ p: '10px', color: 'var(--color-neutral-3)' }}
-        aria-label="directions"
-      >
-        <CloseIcon onClick={() => setValue?.('')} />
-      </IconButton>
-    </Paper>
+      <StyledDivider orientation="vertical" />
+      <StyledIconButton aria-label="directions">
+        <CgClose onClick={() => setValue?.('')} />
+      </StyledIconButton>
+    </StyledPaper>
   );
 };
 
-export default MainInput;
+export default Input;
