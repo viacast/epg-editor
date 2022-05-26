a6e62c55ef07750229676069c796042078fb7ad9
gabrieljablonski <gabriel@viacast.tv>
2022-05-26 16:51:36 -0300

    fileinput component

---
+++ src/components/FileInput/index.tsx
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+
+import Input, { InputProps } from 'components/Input';
+
+export interface FileInputRefProps {
+  click?: () => void;
+}
+
+export interface FileInputProps {
+  forwardRef?: React.MutableRefObject<FileInputRefProps>;
+  onFileUpload?: (files: FileList) => void;
+}
+
+const FileInput: React.FC<InputProps & FileInputProps> = ({
+  forwardRef,
+  onFileUpload,
+  ...rest
+}) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  if (forwardRef?.current) {
+    // eslint-disable-next-line no-param-reassign
+    forwardRef.current.click = () => fileInputRef.current?.click();
+  }
+
+  return (
+    <>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Input {...rest} />
+      <input
+        style={{ display: 'none' }}
+        type="file"
+        ref={fileInputRef}
+        onChange={e => {
+          onFileUpload?.(e.target.files ?? new FileList());
+        }}
+      />
+    </>
+  );
+};
+
+FileInput.defaultProps = {
+  forwardRef: undefined,
+  onFileUpload: undefined,
+};
+
+export default FileInput;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
-import BaseButton from 'components/Button';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { RiMenuAddFill } from 'react-icons/ri';
-import { FaFileExport } from 'react-icons/fa';
-import InputButton from 'components/InputButton';
+import { FaDownload, FaFileExport } from 'react-icons/fa';
+
+import Button from 'components/Button';
+import FileInput, { FileInputRefProps } from 'components/FileInput';
 import { Container as HeaderContainer, Select, Text } from './styles';
 
 const Header: React.FC = () => {
   const { t, i18n } = useTranslation();
   const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
   const [programCount, setProgramCount] = useState(0);
+  const [filename, setFilename] = useState('');
+
+  const fileInputRef = useRef<FileInputRefProps>({});
 
   const handleChange = useCallback(
     evt => {
@@ -22,18 +26,32 @@ const Header: React.FC = () => {
 
   return (
     <HeaderContainer className="no-user-select">
-      <InputButton />
-      <BaseButton
-        text={`${t('header:buttonExportProgram')}`}
-        icon={<FaFileExport />}
+      <FileInput
+        forwardRef={fileInputRef}
+        disabled
+        placeholder={t('header:placeholderInput')}
+        value={filename}
+        setValue={setFilename}
+        onFileUpload={files => {
+          if (!files.length) {
+            return;
+          }
+          setFilename(files[0].name);
+        }}
       />
+      <Button
+        text={t('header:buttonImportProgram')}
+        icon={<FaDownload />}
+        onClick={() => fileInputRef?.current.click?.()}
+      />
+      <Button text={t('header:buttonExportProgram')} icon={<FaFileExport />} />
       <Text>
         {t('header:labelProgram', {
           count: programCount,
         })}
       </Text>
-      <BaseButton
-        text={`${t('header:buttonAddProgram')}`}
+      <Button
+        text={t('header:buttonAddProgram')}
         icon={<RiMenuAddFill />}
         onClick={() => setProgramCount(c => c + 1)}
       />
