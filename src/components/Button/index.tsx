d94609bc9e4f2ea77f11dcd3feb5943483cf165d
Gustavo B Machado <43426219+gusbmachado@users.noreply.github.com>
2022-05-26 10:56:34 -0300

    Update src/components/Button/index.tsx
    Co-authored-by: Gabriel Jablonski <gabriel@viacast.tv>

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { styled } from '@mui/material/styles';
 import Button, { ButtonProps } from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
