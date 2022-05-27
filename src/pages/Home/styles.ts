61cca7a828cd161ead1eaecb0c31f6b3f8a6d88c
gusbmachado <gustavobmachado105@gmail.com>
2022-05-27 14:33:25 -0300

    Homepage Styles Fixed

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,23 +6,19 @@ export const Container = styled.div`
 `;
 
 export const HeaderContainer = styled.div`
-  padding: 81px 50px 0px 50px;
+  padding: 81px 0px 0px 50px;
   display: flex;
   height: fit-content;
-  width: 100%;
+  width: fit-content;
 `;
 
 export const TableContainer = styled.div`
   padding: 30px 50px 81px 50px;
   display: flex;
-  height: 850px;
+  height: 87.1vh;
   width: 100%;
+  max-height: 850px;
+  min-height: 300px;
   font-family: Nunito, sans-serif;
-  color: var(--color-neutral-3);
-`;
-
-export const Form = styled.form`
-  display: flex;
-  width: fit-content;
   color: var(--color-neutral-3);
 `;
