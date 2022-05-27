9d99dbe6c15968ced4f0b7f580ffe3e053425f6b
gusbmachado <gustavobmachado105@gmail.com>
2022-05-27 10:43:41 -0300

    Export Components Fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
-import { Button, Input, FilledInput, Table } from '@mui/material';
+import Button from './Button';
+import FileInput from './FileInput';
+import Input from './Input';
+import ProgramTable from './ProgramTable';
 import ToastContainer from './ToastContainer';
 
-export { Button, Input, FilledInput, Table, ToastContainer };
+export { Button, Input, FileInput, ProgramTable, ToastContainer };
