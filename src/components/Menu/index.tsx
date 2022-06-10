5724443fd9f8a0e1b14286788b64d2a056388af2
gusbmachado <gustavobmachado105@gmail.com>
2022-06-10 17:59:39 -0300

    Menu Component Update

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,16 +1,20 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-self-assign */
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Text, Button, Input, SelectRate } from 'components';
 import { CgClose } from 'react-icons/cg';
 import { AiOutlineSave } from 'react-icons/ai';
-import DatePicker from 'components/Pickers/DatePicker';
-import TimePicker from 'components/Pickers/TimePicker';
-import DurationPickers from 'components/Pickers/DurationPicker';
+import {
+  Text,
+  Button,
+  Input,
+  SelectRate,
+  DatePickers,
+  TimePickers,
+  DurationPickers,
+} from 'components';
 
 import { Program, ProgramRating } from 'services/epg';
+import structuredClone from '@ungap/structured-clone';
+import { emptyProgram } from 'services/epg/program';
 import {
   BottomContainer,
   ButtonContainer,
@@ -27,7 +31,8 @@ export interface MenuProps extends MenuStyleProps {
   overflowStatus;
   programs: Program[];
   selectedProgramId: string;
-  setisClosing: (programId: boolean) => void;
+  setIsClosing: (programId: boolean) => void;
+  onSaveProgram: (newprogram: Program) => void;
 }
 
 const Menu: React.FC<MenuProps> = ({
@@ -35,43 +40,24 @@ const Menu: React.FC<MenuProps> = ({
   overflowStatus,
   programs,
   selectedProgramId,
-  setisClosing,
+  setIsClosing,
+  onSaveProgram,
 }) => {
   const { t } = useTranslation();
 
-  const [program, setProgram] = React.useState<Program[]>(programs);
+  const program = programs.find(p => p.id === selectedProgramId);
+  const [newProgram, setNewProgram] = useState<Program>(
+    program ? structuredClone(program) : emptyProgram(),
+  );
 
-  let i = 0;
-  let aux;
-  let title;
-  let editTitle;
-  let description;
-  let rating;
-  let defaultRate;
-  let date;
-  let time;
-  let timeLength;
-
-  while (i < programs.length) {
-    if (programs[i].id === selectedProgramId) {
-      aux = i;
-      title = programs[i].title;
-      editTitle = title.toString();
-      description = programs[i].description;
-      rating = programs[i].rating;
-      defaultRate = rating.toString();
-      date = new Date(programs[i].startDate);
-      time = new Date(programs[i].startHour);
-      timeLength = programs[i].duration;
-    }
-    // eslint-disable-next-line no-plusplus
-    ++i;
-  }
+  useEffect(() => {
+    setNewProgram(program ? structuredClone(program) : emptyProgram());
+  }, [program, selectedProgramId]);
 
   return (
     <MenuContainer minWidth={minWidth} overflowStatus={overflowStatus}>
       <Toolbar>
-        {t('menu:edit')} <p>{editTitle}</p>
+        {t('menu:edit')}: <p>{newProgram?.title}</p>
       </Toolbar>
       <ContentContainer>
         <BottomContainer>
@@ -80,7 +66,11 @@ const Menu: React.FC<MenuProps> = ({
               <Text noSelect fontFamily="Nunito Bold" fontSize="32px">
                 {t('menu:title')}
               </Text>
-              <Input height="45px" defaultValue={title} disabled={false} />
+              <Input
+                height="45px"
+                value={newProgram?.title}
+                setValue={title => setNewProgram(p => ({ ...p, title }))}
+              />
               <Text noSelect fontFamily="Nunito Bold" fontSize="32px">
                 {t('menu:description')}
               </Text>
@@ -88,31 +78,56 @@ const Menu: React.FC<MenuProps> = ({
                 multiline
                 maxRows={4}
                 height="130px"
-                defaultValue={description}
-                disabled={false}
+                value={newProgram?.description}
+                setValue={description =>
+                  setNewProgram(p => ({ ...p, description }))
+                }
               />
               <Text noSelect fontFamily="Nunito Bold" fontSize="32px">
                 {t('menu:parentalRating')}
               </Text>
-              <SelectRate defaultValue={defaultRate} />
+              <SelectRate
+                value={newProgram?.rating.toString() ?? 'RL'}
+                setValue={rating => {
+                  setNewProgram(p => ({
+                    ...p,
+                    rating: rating as ProgramRating,
+                  }));
+                }}
+              />
               <div style={{ display: 'inline-flex', width: '100%' }}>
                 <div style={{ width: '50%', float: 'left' }}>
                   <Text noSelect fontFamily="Nunito Bold" fontSize="32px">
                     {t('menu:date')}
                   </Text>
-                  <DatePicker programDate={date} />
+                  <DatePickers
+                    date={newProgram?.startDate ?? new Date()}
+                    onDateChange={startDate =>
+                      setNewProgram(p => ({ ...p, startDate }))
+                    }
+                  />
                 </div>
                 <div style={{ width: '50%', marginLeft: '30px' }}>
                   <Text noSelect fontFamily="Nunito Bold" fontSize="32px">
                     {t('menu:time')}
                   </Text>
-                  <TimePicker programTime={time} />
+                  <TimePickers
+                    time={newProgram?.startHour ?? new Date()}
+                    onTimeChange={startHour =>
+                      setNewProgram(p => ({ ...p, startHour }))
+                    }
+                  />
                 </div>
               </div>
               <Text noSelect fontFamily="Nunito Bold" fontSize="32px">
                 {t('menu:length')}
               </Text>
-              <DurationPickers programDuration={timeLength} />
+              <DurationPickers
+                duration={newProgram?.duration ?? 0}
+                onDurationChange={duration =>
+                  setNewProgram(p => ({ ...p, duration }))
+                }
+              />
             </Form>
           </FormContainer>
         </BottomContainer>
@@ -122,7 +137,7 @@ const Menu: React.FC<MenuProps> = ({
             text={t('menu:cancel')}
             icon={<CgClose />}
             onClick={() => {
-              setisClosing(true);
+              setIsClosing(true);
             }}
           />
           <Button
@@ -130,51 +145,8 @@ const Menu: React.FC<MenuProps> = ({
             text={t('menu:save')}
             icon={<AiOutlineSave />}
             onClick={() => {
-              let desc;
-              if (document.getElementsByTagName('textarea')[0] !== undefined) {
-                desc = document.getElementsByTagName('textarea')[0]?.innerHTML;
-              }
-              let dur;
-              time = document.getElementsByTagName('input')[6].value;
-              // eslint-disable-next-line no-restricted-globals
-              if (isNaN(dur)) {
-                dur =
-                  time.split('hour')[0] * 3600 +
-                  time.split('hour')[1].split('minutes')[0] * 60;
-              } else {
-                dur =
-                  time.split('hours')[0] * 3600 +
-                  time.split('hours')[1].split('minutes')[0] * 60;
-              }
-              const hour = new Date();
-              const instant = document.getElementsByTagName('input')[5].value;
-              hour.setHours(
-                Number(instant.split(':')[0]),
-                Number(instant.split(':')[1]),
-                Number(instant.split(':')[2]),
-              );
-              const rate = document.getElementsByTagName('input')[3].value;
-              const ratings = {
-                RL: ProgramRating.RL,
-                R10: ProgramRating.R10,
-                R12: ProgramRating.R12,
-                R14: ProgramRating.R14,
-                R16: ProgramRating.R16,
-                R18: ProgramRating.R18,
-              };
-              programs[aux].title =
-                document.getElementsByTagName('input')[2].value;
-              programs[aux].description = desc;
-              programs[aux].rating = ratings[rate];
-              programs[aux].startDate = new Date(
-                document.getElementsByTagName('input')[4].value,
-              );
-              programs[aux].startHour = new Date(hour);
-              programs[aux].duration = Number(dur);
-              setProgram(programs);
-              // console.log(aux);
-              // console.log(program);
-              setisClosing(true);
+              if (newProgram) onSaveProgram(newProgram);
+              setIsClosing(true);
             }}
           />
         </ButtonContainer>
