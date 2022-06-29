26c5666378804dcb46dc69c34ec807bd6cf3a541
gabrieljablonski <gabriel@viacast.tv>
2022-06-29 10:50:05 -0300

    base epg formatter

---
+++ src/services/epg/formatter.ts
@@ -0,0 +1,13 @@
+import { Program } from './program';
+
+export default class EPGFormatter {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static formatXml(programs: Program[]): string {
+    throw new Error('not implemented');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static formatCsv(programs: Program[]): string {
+    throw new Error('not implemented');
+  }
+}
