5e8c4dc138b55eaab472054686c48c061a114993
gabrieljablonski <gabriel@viacast.tv>
2022-07-05 17:26:25 -0300

    scroll added program into view

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,8 @@
 import useLocalStorage, { LocalStorageKeys } from './useLocalStorage';
 import useMounted from './useMounted';
 import useClickOutside from './useClickOutside';
+import useScrollIntoView from './useScrollIntoView';
 
-export { useLocalStorage, useMounted, useClickOutside };
+export { useLocalStorage, useMounted, useClickOutside, useScrollIntoView };
 
 export { LocalStorageKeys };
