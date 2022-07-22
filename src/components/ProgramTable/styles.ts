dd96d21381c836e3436eed7bd5da90e78314de0a
gusbmachado <gustavobmachado105@gmail.com>
2022-07-22 15:32:32 -0300

    icons hover style

--- src/components/ProgramTable/styles.ts
+++ src/components/ProgramTable/styles.ts
@@ -92,6 +92,11 @@ export const StyledTableRow = styledmui(TableRow)<{ selected?: boolean }>`
     svg, input {
       display: inline-flex;
     }
+    svg {
+      &:hover {
+        opacity: .5;
+      }
+    }
   }
 `;
 
