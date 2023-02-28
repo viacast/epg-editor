2e4dd72f7a6a7a53df1cb1a61330640952acf0d7
Lucas Medeiros <luckcfm@gmail.com>
2023-02-28 15:06:21 -0300

    fixing package.json and deploy

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -9,4 +9,4 @@ timestamp() {
 build_file=epg-editor-build-$(timestamp).tar
 tar -zcvf $build_file -C build/ .
 scp -P 10522 $build_file root@$HOST:/tmp/
-ssh root@$HOST -p 10522 "cd /var/www/html/wordpress/epg/ && rm -r /var/www/html/wordpress/epg/* && tar -xvf /tmp/$build_file"
+ssh root@$HOST -p 10522 "cd /var/www/html/wordpress/epg/ && rm -r /var/www/html/wordpress/epg/* && tar -xvf /tmp/$build_file && mv index.html index.php"
